de99646f3b85a382601869d49477122d2012afbc
Tom Gurion <tgurion@mintel.com>
2022-12-06 15:39:52 +0000

    Cleanup and 30 seconds timer

--- app.py
+++ app.py
@@ -6,13 +6,22 @@ from flask import request
 
 app = Flask(__name__)
 
+TIMER_DURATION = 30
 
 # Dict from name to joining time in seconds since epoch
 available_people = {}
 
 
+def cleanup():
+    now = time.time()
+    to_remove = [x for x, t in available_people.items() if now - t > TIMER_DURATION]
+    for name in to_remove:
+        available_people.pop(name)
+
+
 @app.route("/")
 def index():
+    cleanup()
     name = request.args.get('name')
     if name and not name in available_people:
         available_people[name] = time.time()
@@ -21,6 +30,7 @@ def index():
 
 @app.route("/poll/<name>")
 def poll(name):
+    cleanup()
     if len(available_people) > 1:
         for match in available_people:
             if match != name:
